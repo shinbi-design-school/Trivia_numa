3ee226de5e1f7046c3d12833b46f5b83253c5ec5
katayama38 <ora.u_tan@me.com>
2024-10-23 12:50:14 +0900

    ジャンル別クイズの作成済み。確認ヨロです

---
+++ src/main/java/com/design_shinbi/trivia_numa/model/programming/Html_Css.java
@@ -0,0 +1,5 @@
+package com.design_shinbi.trivia_numa.model.programming;
+
+public class Html_Css {
+
+}
---
+++ src/main/java/com/design_shinbi/trivia_numa/model/programming/Java_F.java
@@ -0,0 +1,5 @@
+package com.design_shinbi.trivia_numa.model.programming;
+
+public class Java_F {
+
+}
