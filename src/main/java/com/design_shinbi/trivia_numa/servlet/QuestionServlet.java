9f28ea5ddedd0fcb09e50d2318f44c56c13e70f2
iihoshiai <hositohuusenn@gmail.com>
2024-10-24 12:48:45 +0900

    Add files via upload

--- src/main/java/com/design_shinbi/trivia_numa/servlet/QuestionServlet.java
+++ src/main/java/com/design_shinbi/trivia_numa/servlet/QuestionServlet.java
@@ -1,15 +1,21 @@
 package com.design_shinbi.trivia_numa.servlet;
 
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
+
+import com.design_shinbi.trivia_numa.model.AnimalQuiz;
+import com.design_shinbi.trivia_numa.model.Quiz;
 
 /**
  * Servlet implementation class QuestionServlet
  */
+
 public class QuestionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -33,8 +39,45 @@ public class QuestionServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		String Genre = request.getParameter("Genre");
+		Quiz quizs =null;
+		switch (Genre){
+			case "AnimalQuiz":
+			 quizs = new AnimalQuiz();
+			 break;
+			case "BaseballQuiz":
+				 quizs = new BaseballQuiz();
+				 break;
+			case "EntertainmentQuiz":
+				 quizs = new EntertainmentQuiz();
+				 break;
+			case "PhotographQuiz":
+				 quizs = new PhotographQuiz();
+				 break;
+			case "SoccerQuiz":
+				 quizs = new SoccerQuiz();
+				 break;
+			case "Html_Css":
+				 quizs = new Html_Css();
+				 break;
+			case "JavaScript":
+				 quizs = new JavaScript();
+				 break;
+			case "Java_F":
+				 quizs = new Java_F();
+				 break;
+			case "Jsp_Servlet":
+				 quizs = new Jsp_Servlet();
+				 break;
+			case "Mysql":
+				 quizs = new Mysql();
+				 break;
+		}
+		session.setAttribute("quizs", quizs);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("question.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
