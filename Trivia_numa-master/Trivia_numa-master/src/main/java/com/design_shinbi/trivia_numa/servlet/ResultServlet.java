f1589549cb32b71d61e11371aa63f48f93ef098e
katayama38 <ora.u_tan@me.com>
2024-10-24 11:17:56 +0900

    出来たのか？？？

---
+++ Trivia_numa-master/Trivia_numa-master/src/main/java/com/design_shinbi/trivia_numa/servlet/ResultServlet.java
@@ -0,0 +1,32 @@
+package com.design_shinbi.trivia_numa.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class ResultServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public ResultServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
