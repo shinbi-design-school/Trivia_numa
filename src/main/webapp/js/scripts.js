c828cb2fa166370dad7591ce7e8b762a8a14c27e
katayama38 <ora.u_tan@me.com>
2024-10-29 09:31:06 +0900

    Update scripts.js

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -1,3 +1,8 @@
-/**
- * 
- */
+document.getElementById("hamburger").addEventListener("click", function() {
+    const nav = document.getElementById("nav");
+    if (nav.style.display === "flex") {
+        nav.style.display = "none"; // 非表示
+    } else {
+        nav.style.display = "flex"; // 表示
+    }
+});
