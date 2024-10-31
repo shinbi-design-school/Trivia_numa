7a86a1642bc57a0a2c271097ebb1a8cac783adc7
kazuya-ship-it <kasu000maru@gmail.com>
2024-10-31 14:52:16 +0900

    Add files via upload

---
+++ A.java
@@ -0,0 +1,21 @@
+package a;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//答えがあってるときも文字が入力される。
+		
+		String a =("123456");
+		
+		//文字数カウント
+		int seikai=a.length();
+		
+		int score=seikai*10;
+		
+		System.out.println("あなたの点数は"+score +"点です。");
+
+	}
+
+}
