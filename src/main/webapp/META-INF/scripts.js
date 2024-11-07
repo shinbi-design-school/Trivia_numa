4b20c23890c5dfceaeff9c28e4e7fdb866f64f71
katayama38 <ora.u_tan@me.com>
2024-11-07 12:19:22 +0900

    Delete src/main directory

--- src/main/webapp/META-INF/scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("hamburger").addEventListener("click", function() {
-    const nav = document.getElementById("nav");
-    if (nav.style.display === "flex") {
-        nav.style.display = "none"; // 非表示
-    } else {
-        nav.style.display = "flex"; // 表示
-    }
-});
